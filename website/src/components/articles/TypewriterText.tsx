5f720e909770eabbf3bff44149b15bb1942cb994
Kai Davenport <kaiyadavenport@gmail.com>
2024-05-12 10:37:04 +0100

    add typewriter component

---
+++ website/src/components/articles/TypewriterText.tsx
@@ -0,0 +1,54 @@
+import React, { FC, useEffect, useState } from 'react'
+import bluebird from 'bluebird'
+import { SxProps } from '@mui/system'
+import { TypographyProps } from '@mui/material/Typography'
+import SerifTypography from '../widgets/SerifTypography'
+
+import { getResponsiveSxAmount } from '../../styles'
+import useTypography from '../../hooks/useTypography'
+
+const TypewriterText: FC<TypographyProps & {
+  text: string,
+  // how many milliseonds to wait before starting
+  initialDelay?: number,
+  // min number milliseconds to wait between each character
+  minSpeed?: number,
+  // max number milliseconds we add to minSpeed (between 0 and 100% will be added)
+  randomSpeed?: number,
+}> = ({
+  text,
+  initialDelay = 0,
+  minSpeed = 15,
+  randomSpeed = 45,
+  ...props
+}) => {
+  const [ currentText, setCurrentText ] = useState('')
+
+  useEffect(() => {
+    let active = true
+    const runText = async () => {
+      let currentIndex = 0
+      setCurrentText('')
+      await bluebird.delay(initialDelay)
+      while(active) {
+        currentIndex++
+        setCurrentText(text.slice(0, currentIndex))
+        await bluebird.delay(minSpeed + (Math.random() * randomSpeed))
+      }
+    }
+    runText()
+    return () => {
+      active = false
+    }
+  }, [
+    text,
+  ])
+
+  return (
+    <SerifTypography {...props}>
+      { currentText }
+    </SerifTypography>
+  )
+}
+
+export default TypewriterText
---
+++ website/src/pages/HomeOld.tsx
@@ -0,0 +1,76 @@
+import React, { FC, Fragment } from 'react'
+
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import Quote from '../components/articles/Quote'
+import HeadlineStrapline from '../components/articles/HeadlineStrapline'
+import ArticleRow from '../components/articles/ArticleRow'
+import ExternalLink from '../components/articles/ExternalLink'
+
+import { getResponsiveSxAmount } from '../styles'
+import theme from '../theme'
+import { ARTICLES } from '../data'
+
+const Home: FC = () => {
+  return (
+    <Box>
+      <HeadlineStrapline
+        headline="Trust in politics is eroding."
+        strapline="Politicans say one thing and then do another."
+        headlineVariant="h1"
+        straplineVariant="h6"
+      />
+      <HeadlineStrapline
+        headline={<>
+          Our <ExternalLink color={theme.palette.primary.main} href="https://en.wikipedia.org/wiki/1708_British_general_election">political system</ExternalLink> was invented <ExternalLink color={theme.palette.primary.main} href="https://en.wikipedia.org/wiki/Electricity"><strong>before electricity.</strong></ExternalLink>
+        </> }
+        strapline="Now we have the Internet, blockchains and generative AI."
+        headlineVariant="h3"
+        straplineVariant="h6"
+      />
+      <HeadlineStrapline
+        strapline={<>
+          We <ExternalLink color={theme.palette.primary.main} href="https://en.wikipedia.org/wiki/I_know_that_I_know_nothing">don't presume</ExternalLink> to know the answers; at the same time:
+        </> }
+        straplineVariant="h5"
+        mt={ 3 }
+        mb={ 3 }
+      />
+      <Quote
+        author="[source]"
+        url="https://meta.wikimedia.org/wiki/Cunningham%27s_Law"
+        sx={{
+          mt: getResponsiveSxAmount(2),
+          ml: getResponsiveSxAmount(4),
+          mb: getResponsiveSxAmount(2),
+        }}
+      >
+        The best way to get the right answer on the internet is not to ask a question; it's to post the wrong answer.
+      </Quote>
+      <HeadlineStrapline
+        strapline="Here are some wrong answers to get the conversation started:"
+        straplineVariant="h6"
+        mt={ 1 }
+        mb={ 6 }
+      />
+      <Divider />
+      {
+        ARTICLES.map((article, index) => (
+          <Box key={ index }>
+            <ArticleRow
+              key={ index }
+              article={ article }
+            />
+            {
+              index < ARTICLES.length - 1 && (
+                <Divider />
+              )
+            }
+          </Box>
+        ))
+      }
+    </Box>
+  )
+}
+
+export default Home
